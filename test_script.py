a4927d9befc5d4e5567b354a85a7a254a925aeb1
m.plotitsin <m.plotitsin@mail.ru>
2026-06-29 17:56:51 +0300

    test camera filtrs 3

--- test_script.py
+++ test_script.py
@@ -11,8 +11,8 @@ def capture_pointcloud(save_path="pointcloud.ply", use_filters=True):
     """
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 6)
-    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 6)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 6)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 6)
 
     profile = pipeline.start(config)
 
